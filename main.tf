df9e7f18c739a7ffe2e64f4be28caf5c2337016d
JackR42 <j.roumimper@gmail.com>
2022-08-08 15:56:30 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+### BEGIN KeyVault
+data "azurerm_key_vault" "project" {
+  name                = "keyvaultproject02160333"
+  resource_group_name = "S2-RG-project02-CORE"
+}
+data "azurerm_key_vault_secret" "secret0" {
+  name         = "ARM-RG"
+  key_vault_id = data.azurerm_key_vault.project.id
+}
+### END KeyVault
+
+### BEGIN MAIN
+resource "azurerm_resource_group" "project" {
+  name = data.azurerm_key_vault_secret.secret0.value
+  location = "westeurope"
+}
