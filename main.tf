24b11970a9d6bfeebaf0bf322afd001425812673
JackR42 <j.roumimper@gmail.com>
2022-08-08 16:39:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,14 +11,15 @@ data "azurerm_key_vault" "project" {
   name                = "keyvaultproject02160333"
   resource_group_name = "S2-RG-project02-CORE"
 }
-data "azurerm_key_vault_secret" "secret0" {
-  name         = "ARM-RG"
-  key_vault_id = data.azurerm_key_vault.project.id
-}
+#data "azurerm_key_vault_secret" "secret0" {
+#  name         = "ARM-RG"
+#  key_vault_id = data.azurerm_key_vault.project.id
+#}
 ### END KeyVault
 
 ### BEGIN MAIN
 resource "azurerm_resource_group" "project02" {
-  name = data.azurerm_key_vault_secret.secret0.value
+#  name = data.azurerm_key_vault_secret.secret0.value
+  name = $(ARM_RG)
   location = "westeurope"
 }
